b16a087f29a680b13fb9bbce061f42f7af4463f0
Metalit <48568911+Metalit@users.noreply.github.com>
2022-09-02 15:32:39 -0500

    May cause liveness crash

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,36 @@
 
 #include "System/IO/File.hpp"
 
+#include "GlobalNamespace/PlayerData.hpp"
+
+#include "GlobalNamespace/PlayerAgreements.hpp"
+#include "GlobalNamespace/BeatmapCharacteristicSO.hpp"
+#include "GlobalNamespace/GameplayModifiers.hpp"
+#include "GlobalNamespace/PlayerSpecificSettings.hpp"
+#include "GlobalNamespace/PracticeSettings.hpp"
+#include "GlobalNamespace/PlayerAllOverallStatsData.hpp"
+#include "GlobalNamespace/PlayerLevelStatsData.hpp"
+#include "GlobalNamespace/PlayerMissionStatsData.hpp"
+#include "GlobalNamespace/ColorSchemesSettings.hpp"
+#include "GlobalNamespace/OverrideEnvironmentSettings.hpp"
+#include "GlobalNamespace/MultiplayerModeSettings.hpp"
+
 #include <filesystem>
 
+using namespace GlobalNamespace;
 using namespace System::IO;
 
 static ModInfo modInfo;
+static bool lightsSet = false;
 
 Logger& getLogger() {
     static Logger* logger = new Logger(modInfo);
     return *logger;
+}
+
+Configuration& getConfig() {
+    static Configuration config = Configuration(modInfo);
+    return config;
 }
 
 std::string GetBackupPath() {
@@ -33,6 +54,17 @@ MAKE_HOOK_MATCH(File_WriteAllText, static_cast<void(*)(StringW, StringW)>(&File:
     }
 }
 
+MAKE_HOOK_FIND(PlayerData_ctor, classof(PlayerData*), ".ctor", PlayerData*, StringW playerId, StringW playerName, bool shouldShowTutorialPrompt, bool shouldShow360Warning, bool agreedToEula, bool didSelectLanguage, bool agreedToMultiplayerDisclaimer, bool avatarCreated, int didSelectRegionVersion, PlayerAgreements* playerAgreements, BeatmapDifficulty lastSelectedBeatmapDifficulty, BeatmapCharacteristicSO* lastSelectedBeatmapCharacteristic, GameplayModifiers* gameplayModifiers, PlayerSpecificSettings* playerSpecificSettings, PracticeSettings* practiceSettings, PlayerAllOverallStatsData* playerAllOverallStatsData, List<PlayerLevelStatsData*>* levelsStatsData, List<PlayerMissionStatsData*>* missionsStatsData, List<StringW>* showedMissionHelpIds, List<StringW>* guestPlayerNames, ColorSchemesSettings* colorSchemesSettings, OverrideEnvironmentSettings* overrideEnvironmentSettings, List<StringW>* favoritesLevelIds, MultiplayerModeSettings* multiplayerModeSettings, int currentDlcPromoDisplayCount, StringW currentDlcPromoId) {
+
+    if(!lightsSet) {
+        playerSpecificSettings->environmentEffectsFilterDefaultPreset = EnvironmentEffectsFilterPreset::AllEffects;
+        playerSpecificSettings->environmentEffectsFilterExpertPlusPreset = EnvironmentEffectsFilterPreset::AllEffects;
+        lightsSet = true;
+    }
+
+    return PlayerData_ctor(playerId, playerName, shouldShowTutorialPrompt, shouldShow360Warning, agreedToEula, didSelectLanguage, agreedToMultiplayerDisclaimer, avatarCreated, didSelectRegionVersion, playerAgreements, lastSelectedBeatmapDifficulty, lastSelectedBeatmapCharacteristic, gameplayModifiers, playerSpecificSettings, practiceSettings, playerAllOverallStatsData, levelsStatsData, missionsStatsData, showedMissionHelpIds, guestPlayerNames, colorSchemesSettings, overrideEnvironmentSettings, favoritesLevelIds, multiplayerModeSettings, currentDlcPromoDisplayCount, currentDlcPromoId);
+}
+
 // Called at the early stages of game loading
 extern "C" void setup(ModInfo& info) {
     info.id = MOD_ID;
@@ -48,6 +80,13 @@ extern "C" void setup(ModInfo& info) {
         }
     } else
         filesystem::create_directory(GetBackupPath());
+        
+    getConfig().Load();
+    lightsSet = getConfig().config.HasMember("lightsSet");
+    if(!lightsSet) {
+        getConfig().config.AddMember("lightsSet", true, getConfig().config.GetAllocator());
+        getConfig().Write();
+    }
 	
     getLogger().info("Completed setup!");
 }
@@ -58,5 +97,6 @@ extern "C" void load() {
 
     getLogger().info("Installing hooks...");
     INSTALL_HOOK(getLogger(), File_WriteAllText);
+    INSTALL_HOOK(getLogger(), PlayerData_ctor);
     getLogger().info("Installed all hooks!");
 }

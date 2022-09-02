294f34c169573821fb26083a300ad9049acb58a5
Metalit <48568911+Metalit@users.noreply.github.com>
2022-09-02 13:00:50 -0500

    Fixes #3

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "main.hpp"
 
-#include "GlobalNamespace/FileHelpers.hpp"
+#include "System/IO/File.hpp"
 
 #include <filesystem>
 
-using namespace GlobalNamespace;
+using namespace System::IO;
 
 static ModInfo modInfo;
 
@@ -20,16 +20,15 @@ std::string GetBackupPath() {
 
 const auto copyopt = std::filesystem::copy_options::overwrite_existing;
 
-MAKE_HOOK_MATCH(FileHelpers_SaveToJSONFile, static_cast<void(*)(Il2CppObject*, StringW, StringW, StringW)>(&FileHelpers::SaveToJSONFile),
-        void, Il2CppObject* obj, StringW filePath, StringW tempFilePath, StringW backupFilePath) {
+MAKE_HOOK_MATCH(File_WriteAllText, static_cast<void(*)(StringW, StringW)>(&File::WriteAllText), void, StringW path, StringW contents) {
 
-    FileHelpers_SaveToJSONFile(obj, filePath, tempFilePath, backupFilePath);
+    File_WriteAllText(path, contents);
 
-    getLogger().info("File saved, path: %s", filePath.operator std::string().c_str());
+    getLogger().info("File saved, path: %s", static_cast<std::string>(path).c_str());
 
-    if(filePath.starts_with(DATA_PATH)) {
+    if(path.starts_with(DATA_PATH)) {
         getLogger().info("Copying for backup");
-        std::string file = filePath;
+        std::string file = path;
         std::filesystem::copy(file, GetBackupPath() + file.substr(sizeof(DATA_PATH) - 1), copyopt);
     }
 }
@@ -47,13 +46,8 @@ extern "C" void setup(ModInfo& info) {
             if(!filesystem::is_directory(file))
                 filesystem::copy(file, DATA_PATH + file.path().filename().string(), copyopt);
         }
-    } else {
+    } else
         filesystem::create_directory(GetBackupPath());
-        for(auto const& file : filesystem::directory_iterator(DATA_PATH)) {
-            if(!filesystem::is_directory(file))
-                filesystem::copy(file, GetBackupPath() + file.path().filename().string(), copyopt);
-        }
-    }
 	
     getLogger().info("Completed setup!");
 }
@@ -63,6 +57,6 @@ extern "C" void load() {
     il2cpp_functions::Init();
 
     getLogger().info("Installing hooks...");
-    INSTALL_HOOK(getLogger(), FileHelpers_SaveToJSONFile);
+    INSTALL_HOOK(getLogger(), File_WriteAllText);
     getLogger().info("Installed all hooks!");
 }
